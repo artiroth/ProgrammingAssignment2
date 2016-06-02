4f80312b34d5338d32f15bb023ac5fcfd5e95e6f
artiroth <artur.czerwonski@gmail.com>
2016-06-02 09:39:59 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve functions create a special object,
+## that stores a numeric matrix (assumed to be invertible) and cache's its inversion.
+## Before using cacheSolve special "matrix" must be created by calling makeCacheMatrix function.
+## Supplied matrix must be invertible.
+## For the purpose of calculation of the inverse of the matrix the R function solve(X) is used.
 
-## Write a short comment describing this function
 
+## makeCacheMatrix function creates a special "matrix", which is a list containing a function to:
+##      set the value of the matrix
+##      get the value of the matrix
+##      set the value of the inverse
+##      get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        sol <- NULL
+        set <- function(y) {
+                x <<- y
+                sol <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) sol <<- solve
+        getsolve <- function() sol
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve function calculates the inverse of the special "matrix" created with makeCacheMatrix function.
+## It first checks if the inverse has already been calculated. If so, it gets the inverse from the cache and returns it.
+## Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setsolve function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+sol <- x$getsolve()
+        if(!is.null(sol)) {
+                message("getting cached data")
+                return(sol)
+        }
+        data <- x$get()
+        sol <- solve(data, ...)
+        x$setsolve(sol)
+        sol
 }
